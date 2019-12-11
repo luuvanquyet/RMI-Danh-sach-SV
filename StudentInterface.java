020ffa74c7501dbb97a6e31cc29bc19613592001
luuvanquyet <luuvanquyet1998@gmail.com>
2019-12-11 14:08:34 +0700

    Revert "Add files via upload"
    This reverts commit 11630e7749bbe139fcd4e7286bd4f893ce2273be.

--- StudentInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-
-public interface StudentInterface extends Remote {
-	public  ArrayList<Student> addStudent(Student s) throws RemoteException;
-	public ArrayList<Student> getStudentList () throws RemoteException;
-	public ArrayList<Student> getTop3Students () throws RemoteException;
-}
